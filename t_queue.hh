9e825a036eb15b785dabeac55c250de6773c45c9
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2015-10-22 10:26:59 +0000

    Summary: seems to be working

--- t_queue.hh
+++ t_queue.hh
@@ -4,39 +4,84 @@
  */
 #ifndef T_QUEUE_H
 #define T_QUEUE_H
+#ifdef __clang__
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wpadded"
+#endif /* clang */
 
+template <typename T>
 class t_queue {
 private:
-    std::deque<pair_info> p_q;
+    std::queue<T> p_q;
     std::mutex q_mtx;
-    pair_info no_pair;
-    bool closed;  /* This must be set when we have no more data to add */
+    std::condition_variable need_refill;
+    std::mutex refill_mtx;
+    std::atomic<bool> alive_wants_notification;
+    std::atomic<bool> closed;
+    unsigned n;
 public:
-    t_queue () {closed = false;}
+    t_queue () {
+        alive_wants_notification = false;
+        closed = false;
+    }
+
     bool is_closed () {
         return closed;}
+
     void close() {
-        closed = true;}
-    const pair_info &front () {
-        return p_q.front();}
-    std::deque<pair_info>::iterator begin() {
-        return p_q.begin(); }
-    std::deque<pair_info>::iterator end() {
-        if (closed || p_q.size())
-            ; /* do nothing, reorder this stuff */
-        else {
-            std::cout << __func__ << " waiting for end";
-            // set variable waiting
-            //wait
-        }
-        return p_q.end();
+        closed = true;
+        need_refill.notify_one();
     }
+
+    bool empty() {
+        std::lock_guard<std::mutex> lock (q_mtx);
+        return p_q.empty();
+    }
+
+    bool alive () {
+        if (closed)
+            if (empty())
+                return (false);
+        if (empty()) {
+            std::unique_lock<std::mutex> refill_lock(refill_mtx);
+            alive_wants_notification = true;
+            need_refill.wait(refill_lock);
+            if (empty())  // Maybe it really was empty
+                return false;
+        }
+        return true;
+    }
+
+    const T &front() {
+        std::unique_lock<std::mutex> refill_lock(refill_mtx);
+        return p_q.front();
+        //        const T & tmp = p_q.front();
+        //        q_mtx.unlock();
+        //        return tmp;
+    }
+
+    void push(const T t) {
+        {
+            std::lock_guard<std::mutex> lock (q_mtx);
+            p_q.push (t);
+        }
+        if (alive_wants_notification) {
+            alive_wants_notification = false;
+            need_refill.notify_one();
+        }
+    }
+
     size_t size() {
         return p_q.size(); }
+
     void pop () {
-        return p_q.pop_front(); }
-    void push_back(pair_info p_i) {
-        p_q.push_back (p_i); }
+        std::lock_guard<std::mutex> lock (q_mtx);
+        p_q.pop();
+    }
 };
+
+#ifdef __clang__
+#    pragma clang diagnostic pop
+#endif /* clang */
 
 #endif /* T_QUEUE_H */
