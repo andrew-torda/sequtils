52eb27d784591befe200206c7ba07c2d86af7499
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2015-10-24 10:06:45 +0000

    Summary: working well so far.

--- reduce.cc
+++ reduce.cc
@@ -11,7 +11,7 @@
  *   -s discard seeds. 
  */
 
-
+#include <algorithm> /* only for playing with sort. Not really needed */
 #include <cerrno>
 #include <condition_variable>
 #include <cstdlib>
@@ -31,6 +31,8 @@
 
 #include <vector>
 
+#include "distmat_rd.hh"
+
 #include "fseq.hh"
 #include "t_queue.hh"
 using namespace std;
@@ -41,49 +43,54 @@ static const unsigned N_SEQBUF = 512;
 /* ---------------- usage ------------------------------------ */
 static int usage ( const char *progname, const char *s)
 {
-    static const char *u = " infile outfile\n";
+    static const char *u
+        = ": [-sv -a sacred_file] mult_seq_align.msa \
+dist_mat.hat2 outfile.msa n_to_keep\n";
     cerr << progname << s << "\n";
     cerr << progname << u;
-    exit (EXIT_FAILURE);
     return EXIT_FAILURE;
 }
 
 typedef vector<fseq> v_fs;
 /* ---------------- from_queue -------------------------------
- * better tactics are to use an iterator to read from the queue
- * and use my own end() operator.
+ * We have bundles of sequences in a vector. Pull each from
+ * the queue
  */
 static void
 from_queue (t_queue <v_fs> &q_fs, map<string, fseq_prop> &f_map)  {
     while (q_fs.alive()) {
         v_fs v = q_fs.front();
+        q_fs.pop();
         for (v_fs::iterator it = v.begin() ; it != v.end(); it++) {
             fseq fs = *it;
-            fseq_prop f_p (fs);
+            fseq_prop f_p (*it);
             f_map [fs.get_cmmt()] = f_p;
         }
-        q_fs.pop();
     }
 }
 
 /* ---------------- get_seq_list -----------------------------
  * From a multiple sequence alignment, read the sequences and
  * just fill out the information.
+ * If we are going to put this in a thread, we have to catch exceptions
+ * here
  */
-static int
-get_seq_list (map<string, fseq_prop> &f_map, const char *in_fname) {
+static void
+get_seq_list (map<string, fseq_prop> &f_map, const char *in_fname, int *ret) {
     string errmsg = __func__;
     size_t len;
     ifstream infile (in_fname);
     if (!infile) {
         errmsg += ": opening " + string (in_fname) + ": " + strerror(errno) + "\n";
-        throw runtime_error (errmsg);
+        cerr << errmsg;
+        *ret = EXIT_FAILURE;
+        return;
     }
         
     {
         streampos pos = infile.tellg();
-        fseq fseq(infile, 0);     /* get first sequence */
-        len = fseq_prop(fseq).length;
+        fseq fs(infile, 0);     /* get first sequence */
+        len = fs.get_seq().size();
         infile.seekg (pos);
     }
 
@@ -103,7 +110,7 @@ get_seq_list (map<string, fseq_prop> &f_map, const char *in_fname) {
                 n = 0;
             }
         }
-        if (v_fs.size())  /* catch the leftovers */
+        if (v_fs.size())                              /* catch the leftovers */
             q_fs.push (v_fs);
         cout << "read "<< scount << " seqs\n";
     }
@@ -112,8 +119,64 @@ get_seq_list (map<string, fseq_prop> &f_map, const char *in_fname) {
     q_fs.close();
 
     t1.join();
-    return EXIT_SUCCESS;
+    *ret = EXIT_SUCCESS;
+}
 
+/* ---------------- get_sacred -------------------------------
+ * read the file of sacred sequences.
+ * Could be called as a thread, so no exceptions allowed and
+ * put success/failure in the last argument.
+ */
+static void
+get_sacred (const char *sacred_fname, vector<string> &v_sacred, int *sacred_ret)
+{
+    ifstream sac_file (sacred_fname);
+    string s;
+    if (!sac_file) {
+        cerr << __func__ << ": opening " << string (sacred_fname)
+             << ": " << strerror(errno) << "\n";
+        *sacred_ret = EXIT_FAILURE;
+        return;
+    }
+    while (getline (sac_file, s))
+        v_sacred.push_back (s);
+    *sacred_ret = EXIT_SUCCESS;
+    sac_file.close();
+    v_sacred.shrink_to_fit();
+    return;
+}
+static void breaker () {}
+/* ---------------- check_lists ------------------------------
+ */
+static const int
+check_lists ( const map<string, fseq_prop> &f_map, vector<string> &v_cmt)
+{
+    for (vector<string>::iterator it = v_cmt.begin(); it != v_cmt.end(); it++) {
+        if (f_map.find(*it) == f_map.end()) {
+            cerr << "Sequence " << *it << " in distmat file not found\n";
+            return EXIT_FAILURE;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+/* ---------------- mark_sacred ------------------------------
+ * Take each sequence that has been marked as sacred.
+ * Look for it in the f_map. If we find it, mark it as sacred.
+ */
+static int
+mark_sacred (map<string, fseq_prop> &f_map, vector<string> &v_sacred)
+{
+    vector<string>::iterator it = v_sacred.begin() ;
+    int ret = EXIT_SUCCESS;
+    for ( ;it != v_sacred.end(); ++it) {
+        if (f_map.find(*it) == f_map.end()) {
+            cerr << __func__ << "seq starting " << *it << " not found\n";
+            return EXIT_FAILURE;
+        }
+        f_map[*it].make_sacred();
+    }
+    return EXIT_SUCCESS;
 }
 
 /* ---------------- main  ------------------------------------ */
@@ -121,32 +184,81 @@ int
 main (int argc, char *argv[])
 {
     int c;
+    short unsigned verbosity = 0;
+    bool seedflag = false;
     const char *progname = argv[0];
-    bool sflag = false;
-    
-    while ((c = getopt(argc, argv, "s")) != -1) {
+    const char *sacred_fname = nullptr;
+    bool eflag = false;
+    while ((c = getopt(argc, argv, "a:sv")) != -1) {
         switch (c) {
+        case 'a':
+            sacred_fname = optarg;                                     break;     
         case 's':
-            sflag = true;                                        break;
+            seedflag = true;                                           break;
+        case 'v':
+            verbosity++;                                               break;
+        case ':':
+            cerr << argv[0] << "Missing opt argument\n"; eflag = true; break;
         case '?':
-            usage(progname, "");                                 break;
+            cerr << argv[0] << "Unknown option\n";       eflag = true; break;
         }
     }
-    if ((argc - optind) < 2)
+    if (eflag)
+        return (usage(progname, ""));
+
+    optind++;
+    if ((argc - optind) < 5)
         usage (progname, " too few arguments");
-    const char *in_fname = argv[optind++];
-    const char *out_fname = argv[optind];
-    cout << "from " << in_fname << " to " << out_fname << "\n";
+    const char *in_fname    = argv[++optind];
+    const char *dist_fname  = argv[++optind];
+    const char *out_fname   = argv[++optind];
+    const char *to_keep_str = argv[++optind];
+    const unsigned to_keep  = stoul (to_keep_str);
+
+    cout << progname << ": using " << in_fname << " as MSA. "
+         << dist_fname << " as distance matrix. Writing to " << out_fname << "\n";
 
     map<string, fseq_prop> f_map;
+    int gsl_ret;
+    thread gsl_thr (get_seq_list, ref(f_map), in_fname, &gsl_ret);
 
-    try {
-        get_seq_list (f_map, in_fname);
-    } catch (runtime_error &e) {
-        cerr << e.what();
-        return EXIT_FAILURE;
+    vector<string> v_sacred;
+    int sacred_ret;
+    thread sac_thr;
+    if (sacred_fname)
+        sac_thr = thread (get_sacred, sacred_fname, ref(v_sacred), &sacred_ret);
+
+    vector<dist_entry> v_dist;
+    {
+        vector<string> v_cmt;
+        try {
+            read_distmat (dist_fname, v_dist, v_cmt) ;
+        } catch (runtime_error &e) {
+            cerr << e.what();  return EXIT_FAILURE; }
+
+        gsl_thr.join();
+        if (gsl_ret != EXIT_SUCCESS) {
+            cerr << __func__ << " error in get_seq_list\n";
+            return EXIT_FAILURE;
+        }
+        if (check_lists (f_map, v_cmt) == EXIT_FAILURE)
+            return EXIT_FAILURE;
+    } // v_cmt goes out of scope and is cleared
+    
+    if (sacred_fname) {
+        sac_thr.join();
+        if (sacred_ret != EXIT_SUCCESS) {
+            cerr << __func__ << " err reading sacred file from "<< sacred_fname;
+            return EXIT_FAILURE;
+        }
     }
+    if (mark_sacred (f_map, v_sacred) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+    v_sacred.clear();
+    cout << "now compare the lists from the two files\n";
 
+
+    
 #   undef check_the_map_is_ok
 #   define check_the_map_is_ok
 #   ifdef check_the_map_is_ok
@@ -156,6 +268,6 @@ main (int argc, char *argv[])
         cout << "f_map size: "<< f_map.size() << "\n";
 #   endif
 
-
+        
     return EXIT_SUCCESS;
 }
