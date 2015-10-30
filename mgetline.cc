026e3fb13e4f6af72b4949416408c39201b60be0
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2015-10-30 15:06:54 +0000

    Summary: replaced the character by character with a buffered method.

--- mgetline.cc
+++ mgetline.cc
@@ -1,31 +1,50 @@
-#include <cstdio>
 #include <fstream>
-#include <iostream>
 #include <string>
 
 /* ---------------- get_seq_list -----------------------------
  * I found that the string version of getline is not thread
  * safe. Presumably, it has in internal static buffer.
+ * Note that we do not have to append a terminating \0.
+ * We always tell string() how many characters to copy.
+ * This should mean it does not have to search the string for
+ * a null.
  */
-static size_t
+size_t
 mgetline ( std::ifstream& is, std::string& str)
 {
-    size_t n = 0;
-    for (int i = is.get();(i != EOF) && (i != '\n'); i = is.get()) {
-        str += i;
+    static const unsigned BSIZ = 200;
+    size_t ibuf = 0;
+    size_t n    = 0;   /* the count that we return */
+    char buf[BSIZ];
+    str.erase();
+    int c = is.get();
+    while ((c != EOF) && (c != '\n')) {
         n++;
+        buf[ibuf] = c;
+        if (++ibuf == BSIZ) {
+            ibuf = 0;
+            str.append (buf, BSIZ);
+        }
+        c = is.get();
     }
+    if (ibuf)
+        str.append (buf, ibuf);
+#   ifdef slow_version
+        for (int i = is.get();(i != EOF) && (i != '\n'); i = is.get()) {
+            str += i;
+            n++;
+        }
+#   endif /* slow_version */
     return n;
 }
 
-
 #ifdef want_test_of_mgetline
-main()
+#include <iostream>
+main ()
 {
     std::string s;
     std::ifstream infile ("test.txt");
     while (size_t i = mgetline (infile, s)) {
-        s.erase();
         std::cout << s << "\n";
         std::cout << "got " << i << "chars\n";
     }
