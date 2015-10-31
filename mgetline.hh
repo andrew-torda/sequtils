6453fff1b95c97ea22c98246a06533dc3fe30314
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2015-10-31 16:15:58 +0000

    Initial revision

---
+++ mgetline.hh
@@ -0,0 +1,10 @@
+/*
+ * 29 oct 2015
+ * Can only be included after <fstream> and <string>.
+ */
+#ifndef MGETLINE_HH
+#define MGETLINE_HH
+
+size_t mgetline ( std::ifstream& is, std::string& str);
+
+#endif /* MGETLINE_HH */
