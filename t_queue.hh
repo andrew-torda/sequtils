20bbf674b03c541c23383989fc5b3320ad3c7b70
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2015-11-24 17:44:46 +0000

    Summary: Now with internal buffering.

--- t_queue.hh
+++ t_queue.hh
@@ -1,4 +1,4 @@
-/*
+ /*
  * 18 oct 2015
  * Can only be included after <mutex> and <queue>
  * Limited to one writer and reader.
@@ -10,9 +10,12 @@
 #    pragma clang diagnostic ignored "-Wpadded"
 #endif /* clang */
 
+#include <atomic>  /* These three are for threading */
 #include <condition_variable>
-/* #include <iomanip> */  /* only for debugging output */
-#include "delay.hh"
+#include <mutex>
+#include <thread>
+
+/* ---------------- t_queue  --------------------------------- */
 template <typename T>
 class t_queue {
 private:
@@ -22,127 +25,37 @@ private:
     std::mutex throttle_mtx;
     std::condition_variable need_refill;
     std::condition_variable throttled_wait;
-    std::atomic<size_t> atm_size;
+    std::atomic<typename std::queue<T>::size_type> atm_size;
     std::atomic<bool>   alive_wants_notification;
-    std::atomic<bool>   closed;
     std::atomic<bool>   throttled;
+    std::atomic<unsigned char> empty_closed;
+
+
+    std::vector<T> feed_buf;
+    std::vector<T> consum_buf;
+    T p_q_tmp;
     unsigned min_in_q, max_in_q;
-    //  short unsigned n_close, n_wait, n_tot;
+    unsigned max_buf;
+    unsigned feed_cnt, consum_cnt;
+    bool c_buf_was_empty;
 
     bool do_throttling; /* Can the queue be throttled ? */
-
-    bool
-    empty() {
-        if (atm_size == 0)
-            return true;
-        return false;
-    }
-
-    void init() {
-        alive_wants_notification = false;
-        closed = false;
-        throttled = false;
-        atm_size = 0;
-    };
-public:
-    t_queue () {
-        do_throttling = false;
-        init();
-    }
-    t_queue (unsigned min_q, unsigned max_q) {
-        min_in_q = min_q;
-        max_in_q = max_q;
-        do_throttling = true;
-        init();
-    }
-
-    bool
-    is_closed () {
-        return closed;}
-
-    void
-    close() {
-        closed = true;  /* It is possible that the reader was waiting. */
-        std::unique_lock<std::mutex> refill_lock(refill_mtx);
-        need_refill.notify_one(); /* say we are really finished */
-    }
-
-    bool
-    alive () {
-        if (empty()) {
-            if (closed) {
-//              std::cerr << __func__ << " n_tot n_wait n_close" << std::setw (6)<< n_tot << std::setw(6) << n_wait << std::setw(6) << ++n_close << "\n";
-                return false;
-            }
-            if (do_throttling)
-                if (throttled && (atm_size <= min_in_q))
-                    throttled_wait.notify_one();
-            std::unique_lock<std::mutex> refill_lock(refill_mtx);
-            alive_wants_notification = true;
-            need_refill.wait(refill_lock, [this](){return atm_size != 0; });
-            alive_wants_notification = false;
-//          n_wait++;
-        }
-//      n_tot++;
-        return true;
-    }
-
-/*  If I combine front() and pop(), I only have to lock the queue once. */
-/*  Unfortunately, I cannot get it to work if I use a reference to the */
-/*  returned T. This is not tragic, but means I am copying unnecessarily. */
-    const T
-    front_and_pop() {
-        T tmp;
-        {
-            std::lock_guard<std::mutex> lock (q_mtx);
-            tmp = p_q.front();
-            p_q.pop();
-            atm_size = p_q.size();
-        }
-        if (do_throttling)
-            if (throttled && (atm_size <= min_in_q))
-                throttled_wait.notify_one();
-        return tmp;
-    }
-
-    const T &
-    front() {
-        std::lock_guard<std::mutex> lock (q_mtx);
-        return p_q.front();
-    }
+    void init();
     
-    void
-    push(const T t) {
-        {
-            std::lock_guard<std::mutex> lock (q_mtx);
-            p_q.push (t);
-            atm_size = p_q.size();
-        }
-        if (alive_wants_notification) {
-/*          man pages suggest the next lock, but it does not seem necessary */
-/*          std::unique_lock<std::mutex> refill_lock(refill_mtx); */
-            need_refill.notify_one();
-        }
-        if (do_throttling) {
-            if (atm_size >= max_in_q) {
-                std::unique_lock<std::mutex> throttle_lock(throttle_mtx);
-                throttled = true;
-                throttled_wait.wait (throttle_lock);
-                throttled = false;
-            }
-        }
-    }
-
-    void
-    pop () {
-        std::lock_guard<std::mutex> lock (q_mtx);
-        p_q.pop();
-        atm_size = p_q.size();
-    }
+public:
+    t_queue ();
+    t_queue (unsigned min_q, unsigned max_q);
+    t_queue (short unsigned n);
+    t_queue (short unsigned prod_buf_siz, short unsigned consum_buf_size,
+             unsigned min_q, unsigned max_q);
+    void close();
+    bool alive();
+    const T front_and_pop();
+    void push(const T t);
 };
 
 #ifdef __clang__
 #    pragma clang diagnostic pop
 #endif /* clang */
-
+#include "t_queue.tcc"
 #endif /* T_QUEUE_H */
