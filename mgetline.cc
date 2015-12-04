020c3fb85e9829cb309425bfcde8716c1c241540
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2015-12-04 07:59:14 +0000

    Summary: Added mc_getline() which hops over blanks and removes comments.

--- mgetline.cc
+++ mgetline.cc
@@ -1,28 +1,33 @@
 #include <fstream>
 #include <string>
-
+#include <cstring>
 #include "mgetline.hh"
 
-/* ---------------- get_seq_list -----------------------------
- * I found that the string version of getline is not thread
- * safe. Presumably, it has in internal static buffer.
- * Note that we do not have to append a terminating \0.
- * We always tell string() how many characters to copy.
- * This should mean it does not have to search the string for
- * a null.
- * 
- * This version jumps over blank lines.
+
+/* ---------------- mgetline ---------------------------------
+ * This version of getline throws away anything after a comment
+ * character
  */
 unsigned
-mgetline ( std::ifstream& is, std::string& str)
+mc_getline ( std::ifstream& is, std::string& str, const char cmmt)
 {
     static const unsigned BSIZ = 200;
     char buf[BSIZ];
     str.clear();
     bool blank = false;
+    bool do_comment = false;
+    if (cmmt != '\0')
+        do_comment = true;
     do {
         blank = false;
         is.getline (buf, BSIZ);  /* 99.9 % of the time, this is all we do. */
+        if (do_comment) {
+            const size_t len = strlen (buf);
+            const char *end = buf + len;
+            for (char *b = buf; b < end; b++)
+                if (*b == cmmt) {
+                    *b = '\0'; break; }
+        }
         str.append (buf);        /* In the very rare cases that the line is */
         if (is.eof())            /* too long for the buffer, we will enter */
             break;               /* this loop. */
@@ -38,6 +43,24 @@ mgetline ( std::ifstream& is, std::string& str)
     return str.size();
 }
 
+/* ---------------- mgetline ---------------------------------
+ * I found that the string version of getline is not thread
+ * safe. Presumably, it has in internal static buffer.
+ * Note that we do not have to append a terminating \0.
+ * We always tell string() how many characters to copy.
+ * This should mean it does not have to search the string for
+ * a null.
+ * 
+ * This version jumps over blank lines.
+ */
+unsigned
+mgetline ( std::ifstream& is, std::string& str)
+{    
+    return mc_getline (is, str, '\0');
+}
+
+
+/* ---------------- testing  ---------------------------------
 #ifdef want_test_of_mgetline
 #include <iostream>
 main ()
--- mgetline.hh
+++ mgetline.hh
@@ -5,6 +5,6 @@
 #ifndef MGETLINE_HH
 #define MGETLINE_HH
 
-size_t mgetline ( std::ifstream& is, std::string& str);
-
+unsigned mgetline ( std::ifstream& is, std::string& str);
+unsigned mc_getline ( std::ifstream& is, std::string& str, const char cmmt);
 #endif /* MGETLINE_HH */
