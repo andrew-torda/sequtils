807190e5a85ded75e4919a019f965e9ba3427918
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2016-06-21 11:42:31 +0000

    Summary: fixed define for clang

--- regex_prob.hh
+++ regex_prob.hh
@@ -10,14 +10,25 @@
 #ifndef REGEX_PROB_HH
 #define REGEX_PROB_HH
 
-#if (!(defined (__GNUC__)) ||  ((__GNUC__ * 10000 + __GNUC_MINOR__ * 100) >= 40900) || defined __clang__)
+#if defined __SUNPRO_CC  /* even with 12.5 beta, this was broken */
+#   define regex_bust
+#endif
+
+#if defined (__GNUC__) && !defined (__clang__)
+#    if ((__GNUC__ * 10000 + __GNUC_MINOR__ * 100) < 40900)
+#        define regex_bust
+#    endif
+#endif
+
+#if (!(defined (regex_bust)))
 #    include <regex>
 #    define regex_choice std
 #    pragma message "Using regex from standards and not boost"
 #else
 #    include <boost/regex.hpp>
 #    define regex_choice boost
-#    pragma message "Using boost for regex functions"
+#    pragma
+message "Using boost for regex functions"
 #endif /* old gcc */
 
 
